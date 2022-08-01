94f6f5347b6d96978fcd8357d34d4e32e7fd1e1e
Admin <Admin@DESKTOP-H9G125C>
2022-08-01 04:15:40 -0700

    Pull request trial

---
+++ src/com/infy/firstProject/github_Trial/Trial_fiestProject.java
@@ -0,0 +1,10 @@
+package com.infy.firstProject.github_Trial;
+
+public class Trial_fiestProject
+{
+	public static void main(String args [])
+	{
+		System.out.println("Pull request trial");
+	}
+
+}
